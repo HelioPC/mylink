030e956189aa524aa3b67eb4b4739dda514b8400
Eliúde Vemba <sweeteliude@gmail.com>
2022-12-13 01:02:04 +0100

    Set build port (4173) 🚪

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/mylink/'
+  base: '/mylink/',
+  server: {
+    port: 4173,
+    open: false,
+    strictPort: true,
+    host: true
+  }
 })
